54fd3e332bd081b7a14083dca9b3ac7800db4693
againHummmus <hummmus.tsx@yandex.com>
2025-05-18 19:28:54 +0300

    master - edit basic auth

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+// middleware.ts
+import { createNextAuthMiddleware } from 'nextjs-basic-auth-middleware'
+
+export const middleware = createNextAuthMiddleware({
+})
 
 export const config = {
-  matcher: ["/", "/index"],
-};
-
-export function middleware(req: NextRequest) {
-  const { ip } = req;
-  const basicAuth = req.headers.get("authorization");
-  const url = req.nextUrl;
-
-  if (
-    process.env.IS_PRODUCTION === "false" &&
-    ip !== "xx.xxx.xxx.xxx"
-  ) {
-    if (basicAuth) {
-      const authValue = basicAuth.split(" ")[1];
-      const [user, pwd] = atob(authValue).split(":");
-
-      const validUser = process.env.BASIC_AUTH_USER;
-      const validPassWord = process.env.BASIC_AUTH_PASS;
-
-      if (user === validUser && pwd === validPassWord) {
-        return NextResponse.next();
-      }
-    }
-    url.pathname = "/api/basicAuth";
-
-    return NextResponse.rewrite(url);
-  }
+  matcher: [
+    '/*',
+  ]
 }
